59fea12fa3331a4b824f39fc5f003b424e21f003
adelbentakouka <ad.bentakouka@gmail.com>
2019-01-22 08:46:17 +0100

    aaa

---
+++ AssistanceMessagerie/src/assistancemessagerie/zdc.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assistancemessagerie;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author p1819346
+ */
+public class zdc {
+    public ArrayList Listeroom = new ArrayList();
+}
